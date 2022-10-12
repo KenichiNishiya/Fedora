0a0c6df25307eff36590a8d7736c96d2a088dea6
KenichiNishiya <yorimitsu@proton.me>
2022-10-11 21:27:11 -0300

    Updated

--- scripts/baka.sh
+++ scripts/baka.sh
@@ -2,17 +2,16 @@
 
 day=$(date +"%Y-%m-%dT%H%M%S")
 
-cp ~/.config/alacritty/alacritty.yml ~/Documents/dotfiles/alacritty/alacritty.yml-$day
-cp ~/.config/bspwm/bspwmrc ~/Documents/dotfiles/bspwm/bspwmrc-$day
-cp ~/.config/dunst/dunstrc ~/Documents/dotfiles/dunst/dunstrc-$day
-cp ~/.config/picom/picom.conf ~/Documents/dotfiles/picom/picom.conf-$day
-cp ~/.config/polybar/config ~/Documents/dotfiles/polybar/config-$day
-cp ~/.config/polybar/launch.sh ~/Documents/dotfiles/polybar/lauch.sh-$day
-cp ~/.config/rofi/config.rasi ~/Documents/dotfiles/rofi/config.rasi-$day
-cp ~/.config/sxhkd/sxhkdrc ~/Documents/dotfiles/sxhkd/sxhkdrc-$day
-cp ~/.vimrc ~/Documents/dotfiles/vim/vimrc-$day
-cp ~/.zsh_aliases ~/Documents/dotfiles/zsh_aliases/zsh_aliases-$day
-cp ~/.profile ~/Documents/dotfiles/profile/profile-$day
-cp ~/.zshrc ~/Documents/dotfiles/zshrc/zshrc-$day
-cp ~/Documents/arsh.sh ~/Documents/dotfiles/arsh/arsh-$day
+cp ~/.config/alacritty/alacritty.yml ~/Documents/bakap/alacritty/alacritty.yml-$day
+cp ~/.config/bspwm/bspwmrc ~/Documents/bakap/bspwm/bspwmrc-$day
+cp ~/.config/dunst/dunstrc ~/Documents/bakap/dunst/dunstrc-$day
+cp ~/.config/polybar/config ~/Documents/bakap/polybar/config-$day
+cp ~/.config/polybar/launch.sh ~/Documents/bakap/polybar/lauch.sh-$day
+cp ~/.config/rofi/config.rasi ~/Documents/bakap/rofi/config.rasi-$day
+cp ~/.config/sxhkd/sxhkdrc ~/Documents/bakap/sxhkd/sxhkdrc-$day
+cp ~/.config/nvim/init.vim ~/Documents/bakap/nvim/init.vim-$day
+cp ~/.zsh_aliases ~/Documents/bakap/zsh_aliases/zsh_aliases-$day
+cp ~/.profile ~/Documents/bakap/profile/profile-$day
+cp ~/.zshrc ~/Documents/bakap/zshrc/zshrc-$day
+cp ~/Documents/fedora.sh ~/Documents/bakap/fedora/fedora.sh-$day
 echo "Bakad up dot flies"
